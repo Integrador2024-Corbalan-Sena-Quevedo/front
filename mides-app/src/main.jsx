f9225efe989a09cdf5edd81066c14a2b1a561844
FeliSena <felipesena1025@gmail.com>
2024-05-19 19:46:33 -0300

    Upload files

---
+++ mides-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FileUpload from './assets/UploadCSV'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FileUpload />
+  </React.StrictMode>,
+)
