3ff58ecf531ae10d1e7888947e881944079d35aa
Santi0802 <santifabi8@gmail.com>
2024-08-20 16:26:30 -0300

    Front estadísticas

--- mides-app/src/components/Estadisticas.jsx
+++ mides-app/src/components/Estadisticas.jsx
@@ -6,15 +6,31 @@ const Estadisticas = () => {
   const [dataEmpleosEnMontevideo, setEmpleosEnMontevideo] = useState([]);
   const [dataEmpleosFormacion, setEmpleosFormacion] = useState([]);
   const [dataEmpleosCargaHoraria, setEmpleosCargaHoraria] = useState([]);
-  const [selectedStat, setSelectedStat] = useState('operadoresPorRol'); // Estado para manejar la selección
+  const [dataEntrevistas, setEntrevistas] = useState([]);
+  const [dataEntrevistasGenero, setEntrevistasGenero] = useState([]);
+  const [dataCandidatosEdad, setCandidatosEdad] = useState([]);
+  const [dataCandidatosDiscapacidad, setCandidatosDiscapacidad] = useState([]);
+  const [dataCandidatosEducacion, setCandidatosEducacion] = useState([]);
+  const [dataCandidatosCargaHoraria, setCandidatosCargaHoraria] = useState([]);
+  const [selectedStatCandidato, setSelectedStatCandidato] = useState('candidatoGenero'); 
+  const [selectedStatEmpleo, setSelectedStatEmpleo] = useState('empleosDepartamento'); 
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AA336A', '#FF9999', '#FF6666', '#CCCC00', '#CC00CC', '#9900CC'];
+  const COLORS = [
+    '#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AA336A', '#FF9999', '#FF6666', '#CCCC00', '#CC00CC', '#9900CC',
+    '#FF5733', '#C70039', '#900C3F', '#581845', '#2ECC71', '#AF7AC5', '#5499C7', '#F4D03F', '#E74C3C', '#45B39D'
+  ];
 
   useEffect(() => {
     fetchCantidadOperadoresPorRol();
     fetchEmpleosEnMontevideo();
     fetchEmpleosFormacion();
     fetchEmpleosCargaHoraria();
+    fetchEntrevistas();
+    fetchEntrevistasPorGenero();
+    fetchCandidatosEdad();
+    fetchCandidatosDiscapacidad();
+    fetchCandidatosEducacion();
+    fetchCandidatosHoras();
   }, []);
   
   const fetchCantidadOperadoresPorRol = async () => {
@@ -113,7 +129,7 @@ const fetchEmpleosCargaHoraria = async () => {
 
       const data = await response.json();
       const transformedData = Object.keys(data).map(key => ({
-          formacion: key,
+          carga: key,
           cantidad: data[key]
       }));
       setEmpleosCargaHoraria(transformedData);
@@ -122,26 +138,217 @@ const fetchEmpleosCargaHoraria = async () => {
   }
 };
 
+const fetchEntrevistas = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/entrevistasPorAnio', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          anio: key,
+          cantidad: data[key]
+      }));
+      setEntrevistas(transformedData);
+  } catch (error) {
+      console.error('Error al obtener las entrevistas:', error);
+  }
+};
+
+
+const fetchEntrevistasPorGenero = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/entrevistasPorGenero', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          genero: key,
+          cantidad: data[key]
+      }));
+      setEntrevistasGenero(transformedData);
+  } catch (error) {
+      console.error('Error al obtener las entrevistas:', error);
+  }
+};
+
+const fetchCandidatosEdad = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/candidatosPorEdad', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          edad: key,
+          cantidad: data[key]
+      }));
+      setCandidatosEdad(transformedData);
+  } catch (error) {
+      console.error('Error al obtener los candidatos:', error);
+  }
+};
+
+const fetchCandidatosDiscapacidad = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/candidatosPorDiscapacidad', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          discapacidad: key,
+          cantidad: data[key]
+      }));
+      setCandidatosDiscapacidad(transformedData);
+  } catch (error) {
+      console.error('Error al obtener los candidatos:', error);
+  }
+};
+
+const fetchCandidatosEducacion = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/candidatosNivelFormacion', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          nivelEducativo: key,
+          cantidad: data[key]
+      }));
+      setCandidatosEducacion(transformedData);
+  } catch (error) {
+      console.error('Error al obtener los candidatos:', error);
+  }
+};
+
+const fetchCandidatosHoras = async () => {
+  const token = localStorage.getItem('token');
+  try {
+      const response = await fetch('http://localhost:8080/estadisticas/candidatosCargaHoraria', {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const transformedData = Object.keys(data).map(key => ({
+          horas: key,
+          cantidad: data[key]
+      }));
+      setCandidatosCargaHoraria(transformedData);
+  } catch (error) {
+      console.error('Error al obtener los candidatos:', error);
+  }
+};
+
+
+
   return (
     <div>
       <h2>Estadísticas de Empleo</h2>
 
-      <select onChange={(e) => setSelectedStat(e.target.value)}>
-        <option value="operadoresPorRol">Distribución de Operadores por Rol</option>
-        <option value="empleosDepartamento">Distribución de Empleos por Departamento</option>
-        <option value="empleosFormacion">Distribución de Empleos por Formación Académica</option>
-        <option value="empleosCargaHoraria">Distribución de Empleos por Carga Horaria</option>
+      
+      
+      <div>
+  <h3>Entrevistas por año</h3>
+  <ResponsiveContainer width="100%" height={400}>
+    <BarChart data={dataEntrevistas}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="anio" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="cantidad" fill="#8884d8">
+        {dataEntrevistas.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Bar>
+    </BarChart>
+  </ResponsiveContainer>
+</div>
+
+
+<select onChange={(e) => setSelectedStatCandidato(e.target.value)}>
+        
+        <option value="candidatoGenero">Distribución de candidatos por género</option>
+        <option value="candidatoEdad">Distribución de candidatos por edad</option>
+        <option value="candidatoDiscapacidad">Distribución de candidatos por discapacidad</option>
+        <option value="candidatoEducacion">Distribución de candidatos por nivel educativo</option>
+        <option value="candidatoHoras">Distribución de candidatos por disposición de carga horaria</option>
+  
       </select>
 
       <div style={{ width: '100%', height: 400 }}>
-        {selectedStat === 'operadoresPorRol' && (
+       
+
+        {selectedStatCandidato === 'candidatoGenero' && (
           <>
-            <h3>Distribución de operadores por rol</h3>
+            <h3>Distribución de candidatos por género</h3>
             <ResponsiveContainer>
               <PieChart>
-                <Pie dataKey="cantidad" data={dataOperadoresPorRol} nameKey="rol" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                <Pie dataKey="cantidad" data={dataEntrevistasGenero} nameKey="genero" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
                   {
-                    dataOperadoresPorRol.map((entry, index) => (
+                    dataEntrevistasGenero.map((entry, index) => (
                       <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                     ))
                   }
@@ -152,7 +359,93 @@ const fetchEmpleosCargaHoraria = async () => {
           </>
         )}
 
-        {selectedStat === 'empleosDepartamento' && (
+{selectedStatCandidato === 'candidatoEdad' && (
+          <>
+            <h3>Distribución de candidatos por edad</h3>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie dataKey="cantidad" data={dataCandidatosEdad} nameKey="edad" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                  {
+                    dataCandidatosEdad.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </>
+        )}
+
+{selectedStatCandidato === 'candidatoDiscapacidad' && (
+          <>
+            <h3>Distribución de candidatos por discapacidad</h3>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie dataKey="cantidad" data={dataCandidatosDiscapacidad} nameKey="discapacidad" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                  {
+                    dataCandidatosDiscapacidad.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </>
+        )}
+
+{selectedStatCandidato === 'candidatoEducacion' && (
+          <>
+            <h3>Distribución de candidatos por nivel educativo</h3>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie dataKey="cantidad" data={dataCandidatosEducacion} nameKey="nivelEducativo" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                  {
+                    dataCandidatosEducacion.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </>
+        )}
+
+{selectedStatCandidato === 'candidatoHoras' && (
+          <>
+            <h3>Distribución de candidatos por disposición de carga horaria</h3>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie dataKey="cantidad" data={dataCandidatosCargaHoraria} nameKey="horas" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                  {
+                    dataCandidatosCargaHoraria.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))
+                  }
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </>
+        )}
+
+
+      </div>
+
+
+      <select onChange={(e) => setSelectedStatEmpleo(e.target.value)}>
+        
+        <option value="empleosDepartamento">Distribución de empleos por departamento</option>
+        <option value="empleosFormacion">Distribución de empleos por formación académica</option>
+        <option value="empleosCargaHoraria">Distribución de empleos por carga horaria</option>
+      </select>
+
+      <div style={{ width: '100%', height: 400 }}>
+       
+
+        {selectedStatEmpleo === 'empleosDepartamento' && (
           <>
             <h3>Distribución de empleos por departamento</h3>
             <ResponsiveContainer>
@@ -170,7 +463,7 @@ const fetchEmpleosCargaHoraria = async () => {
           </>
         )}
 
-        {selectedStat === 'empleosFormacion' && (
+        {selectedStatEmpleo === 'empleosFormacion' && (
           <>
             <h3>Distribución de empleos por formación académica requerida</h3>
             <ResponsiveContainer>
@@ -188,12 +481,12 @@ const fetchEmpleosCargaHoraria = async () => {
           </>
         )}
 
-{selectedStat === 'empleosCargaHoraria' && (
+{selectedStatEmpleo === 'empleosCargaHoraria' && (
           <>
             <h3>Distribución de empleos por carga horaria</h3>
             <ResponsiveContainer>
               <PieChart>
-                <Pie dataKey="cantidad" data={dataEmpleosCargaHoraria} nameKey="formacion" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
+                <Pie dataKey="cantidad" data={dataEmpleosCargaHoraria} nameKey="carga" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label>
                   {
                     dataEmpleosCargaHoraria.map((entry, index) => (
                       <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
