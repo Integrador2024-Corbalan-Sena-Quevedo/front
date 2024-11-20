b167d8d51be6c318226acbe288a870e91dfcd82a
Corb29 <MC277676@fi365.ort.edu.uy>
2024-11-20 20:15:25 -0300

    Eliminacion de console.log

--- mides-app/src/components/useFetchJobs.jsx
+++ mides-app/src/components/useFetchJobs.jsx
@@ -22,7 +22,6 @@ const useFetchJobs = () => {
                 if(response.status === 200){
                     const data = await response.json()
                     setJobs(data)
-                    console.log(data);
                     const activeJobs = data.filter(job => job.activo === 0);                   
                     setJobsActives(activeJobs);
                     setLoading(false);
